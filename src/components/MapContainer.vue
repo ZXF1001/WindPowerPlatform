9513c30ed6445cd13c5ac91559a905ff991eba07
张潇峰 <1809996465@qq.com>
2022-12-10 18:44:14 +0800

    修改了集群列表高度不一致的问题

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -323,9 +323,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.left,
+#container {
+  height: 300px;
+}
 .left {
-  height: 270px;
-
   overflow: scroll;
   overflow-x: hidden;
 }
@@ -333,6 +335,5 @@ export default {
   padding: 0px;
   margin: 0px;
   width: 100%;
-  height: 300px;
 }
 </style>
