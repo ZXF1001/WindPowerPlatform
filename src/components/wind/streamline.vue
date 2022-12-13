e74970c1999e8719fd036f22afdb63500f66e293
张潇峰 <1809996465@qq.com>
2022-12-14 00:43:48 +0800

    把绘制流线图封装成了函数方法

--- src/components/wind/streamline.vue
+++ src/components/wind/streamline.vue
@@ -20,85 +20,69 @@ export default {
     initMap() {
       //瓦片地图列表
       var baseLayers = {
-        AMap1: L.tileLayer(
+        高德卫星: L.tileLayer(
           'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
-          {
-            maxZoom: 18,
-          }
-        ), // 高德卫星
-        TXLW: L.tileLayer(
-          'http://rt0.map.gtimg.com/realtimerender?z={z}&x={x}&y={-y}&type=vector&style=0',
-          {
-            maxZoom: 18,
-          }
-        ), // 腾讯地图路网
-        ArcGIS: L.tileLayer(
-          'https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer/tile/{z}/{y}/{x}',
-          {
-            maxZoom: 18,
-          }
-        ), // ArcGIS
-        AMap2: L.tileLayer(
+          { minZoom: 1, maxZoom: 18 }
+        ),
+        高德路网: L.tileLayer(
           'https://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}',
-          {
-            maxZoom: 18,
-          }
-        ), // 高德路网
-
-        Dark: L.tileLayer(
+          { minZoom: 3, maxZoom: 18 }
+        ),
+        腾讯地图路网: L.tileLayer(
+          'http://rt0.map.gtimg.com/realtimerender?z={z}&x={x}&y={-y}&type=vector&style=0',
+          { minZoom: 3, maxZoom: 18 }
+        ),
+        GeoQ标准: L.tileLayer(
+          'https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer/tile/{z}/{y}/{x}',
+          { minZoom: 0, maxZoom: 18 }
+        ),
+        GeoQ暖色: L.tileLayer(
+          'https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetWarm/MapServer/tile/{z}/{y}/{x}',
+          { minZoom: 0, maxZoom: 18 }
+        ),
+        GeoQ灰色: L.tileLayer(
+          'https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetGray/MapServer/tile/{z}/{y}/{x}',
+          { minZoom: 0, maxZoom: 18 }
+        ),
+        GeoQ深色: L.tileLayer(
           'https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}',
-          {
-            maxZoom: 18,
-          }
-        ), // 暗色
+          { minZoom: 0, maxZoom: 18 }
+        ),
+
         opentopomap: L.tileLayer(
           'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-          {
-            maxZoom: 18,
-          }
-        ), // 密集道路
-        White: L.tileLayer(
+          { minZoom: 0, maxZoom: 18 }
+        ),
+        CARTO白色: L.tileLayer(
           'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-          {
-            maxZoom: 18,
-          }
-        ), // 白色
-        TDT1: L.tileLayer(
-          'https://t3.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TileMatrix={z}&TileRow={y}&TileCol={x}&tk=ec899a50c7830ea2416ca182285236f3',
-          {
-            maxZoom: 18,
-          }
-        ), // 天地图
-        TDT2: L.tileLayer(
-          'http://t0.tianditu.gov.cn/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={z}&TileRow={y}&TileCol={x}&style=default&format=tiles&tk=ec899a50c7830ea2416ca182285236f3',
-          {
-            maxZoom: 18,
-          }
-        ), // 天地图卫星
-        TDT3: L.tileLayer(
-          'http://t0.tianditu.gov.cn/ter_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ter&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TileMatrix={z}&TileRow={y}&TileCol={x}&tk=ec899a50c7830ea2416ca182285236f3',
-          {
-            maxZoom: 18,
-          }
-        ), // 天地图地形
+          { minZoom: 0, maxZoom: 18 }
+        ),
+
+        天地图标准: L.tileLayer(
+          'https://t3.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TileMatrix={z}&TileRow={y}&TileCol={x}&tk=9e888cc799f9e890369bf6c08bba4bf3',
+          { minZoom: 1, maxZoom: 18 }
+        ),
+        天地图卫星: L.tileLayer(
+          'http://t0.tianditu.gov.cn/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={z}&TileRow={y}&TileCol={x}&style=default&format=tiles&tk=9e888cc799f9e890369bf6c08bba4bf3',
+          { minZoom: 1, maxZoom: 18 }
+        ),
+        天地图地形: L.tileLayer(
+          'http://t0.tianditu.gov.cn/ter_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ter&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TileMatrix={z}&TileRow={y}&TileCol={x}&tk=9e888cc799f9e890369bf6c08bba4bf3',
+          { minZoom: 1, maxZoom: 18 }
+        ),
       }
-      //定义地图
-      this.map = L.map('map', {
+      //定义map对象
+      var map = L.map('map', {
         //参考坐标系
         // crs: L.CRS.EPSG3857,
         attributionControl: false,
         zoomControl: false,
         center: [41.05, 114.8],
         zoom: 9,
-        layers: baseLayers.Dark, //默认加载图层
+        layers: baseLayers['GeoQ深色'], //默认加载图层
       })
-      this.map.on('move', (e) => {
-        velocityLayer1.remove()
-      })
-      this.map.on('moveend', (e) => {
-        this.map.addLayer(velocityLayer1)
-      })
-      // this.map.dragging.disable() // 禁止平移
+      // map.dragging.disable() // 禁止平移
+      // 定义图层控件
       var layerControl = L.control
         .layers(
           baseLayers,
@@ -108,25 +92,29 @@ export default {
             collapsed: false,
           }
         )
-        .addTo(this.map)
-
+        .addTo(map)
+      //定义比例尺控件
       L.control
         .scale({
           position: 'bottomleft',
           maxWidth: '100',
           imperial: false,
         })
-        .addTo(this.map)
+        .addTo(map)
       //////////加入风场流线//////////
-      const n = data[0].header.nx * data[0].header.ny //网格数
+      this.drawStream(map, layerControl, data)
+    },
+    drawStream(mapObj, layerControlObj, windData) {
+      //封装的绘制风场流场方法，windData有格式要求
+      const n = windData[0].header.nx * windData[0].header.ny //网格数
       var minMag = Math.sqrt(
-        Math.pow(data[0].data[0], 2) + Math.pow(data[1].data[0], 2)
+        Math.pow(windData[0].data[0], 2) + Math.pow(windData[1].data[0], 2)
       )
       var maxMag = 0
       var mag = 0
       for (var i = 0; i < n; i++) {
         mag = Math.sqrt(
-          Math.pow(data[0].data[i], 2) + Math.pow(data[1].data[i], 2)
+          Math.pow(windData[0].data[i], 2) + Math.pow(windData[1].data[i], 2)
         )
         minMag = mag < minMag ? mag : minMag
         maxMag = mag > maxMag ? mag : maxMag
@@ -143,7 +131,7 @@ export default {
           directionString: '风向',
           speedString: '风速',
         },
-        data: data,
+        data: windData,
         minVelocity: minMag,
         maxVelocity: maxMag,
         velocityScale: 0.01,
@@ -152,8 +140,15 @@ export default {
         // particleMultiplier: 0.001,
         // colorScale:[],
       })
-      this.map.addLayer(velocityLayer1)
-      layerControl.addOverlay(velocityLayer1, 'Wind Streamline')
+      mapObj.addLayer(velocityLayer1)
+      layerControlObj.addOverlay(velocityLayer1, 'Wind Streamline')
+      //挂载map对象移动事件（隐藏流线）
+      mapObj.on('move', () => {
+        velocityLayer1.remove()
+      })
+      mapObj.on('moveend', () => {
+        mapObj.addLayer(velocityLayer1)
+      })
     },
   },
   mounted() {
