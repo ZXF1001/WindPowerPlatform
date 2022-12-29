95cbbca414a70154e4930a21f1ad83e5d146856f
张潇峰 <1809996465@qq.com>
2022-12-29 23:17:23 +0800

    设置全局字体

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,10 @@ body,
   padding: 0;
   height: 100%;
 }
+* {
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+}
 .scroller::-webkit-scrollbar {
   width: 8px;
   height: 9px;
