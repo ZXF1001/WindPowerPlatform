e63c8e1667d56307384780ea158292d5173d1d61
张潇峰 <1809996465@qq.com>
2023-01-07 11:57:05 +0800

    小改进

--- src/api/wind/getWeather.js
+++ src/api/wind/getWeather.js
@@ -1,4 +1,3 @@
-import httpOpenWeather from "@/utils/http/requestOpenWeather.js";
 import httpQweather from "@/utils/http/requestQweather.js";
 //*请求和风天气数据grid-weather/now?
 export const getQ24hWeather = (lat, lng) => {
@@ -8,13 +7,4 @@ export const getQ24hWeather = (lat, lng) => {
 export const getQNowWeather = (lat, lng) => {
   var url = `/now?location=${lng},${lat}&key=a1d4666305e340d2949e735377311cca`;
   return httpQweather.get(url);
-};
-//请求openweather天气数据
-export const getOpen5DayWeather = (lat, lon) => {
-  var url = `forecast?lat=${lat}&lon=${lon}&units=metric&appid=085fd907491d01041328b45abec2f7ac`;
-  return httpOpenWeather.get(url);
-};
-export const getOpenNowWeather = (lat, lon) => {
-  var url = `weather?lat=${lat}&lon=${lon}&units=metric&appid=085fd907491d01041328b45abec2f7ac`;
-  return httpOpenWeather.get(url);
 };
