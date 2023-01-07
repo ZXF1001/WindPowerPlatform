0917a053311a4165c2debb101e5a6da434d29a90
张潇峰 <1809996465@qq.com>
2023-01-07 18:58:28 +0800

    修改新的baseURL

--- src/utils/http/requestMyApi.js
+++ src/utils/http/requestMyApi.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const http = axios.create({
   // 通用请求地址前缀
-  baseURL: "http://1.117.224.40:8081/api",
+  baseURL: "http://1.117.224.40/api",
   // headers: { "X-Requested-With": "XMLHttpRequest" },
-  timeout: 30000,
+  timeout: 15000,
 });
 
 // 添加请求拦截器
