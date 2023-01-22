03c644292bed51d97b77440ccba7cdbb339c72e5
张潇峰 <1809996465@qq.com>
2023-01-22 19:51:58 +0800

    基本实现地形显示

--- src/components/terrain/test.vue
+++ src/components/terrain/test.vue
@@ -19,19 +19,25 @@ export default {
       renderer: null,
       mesh: null,
       texture: null,
+      animationID: null,
     }
   },
   methods: {
     test() {
-      readRemoteCSV('http://1.117.224.40/DEM/Terrain.csv', (res) => {
+      readRemoteCSV('http://localhost:8080/example/DEM/2dDEM.csv', (res) => {
         console.log(res)
       })
     },
     fetchData() {
-      readRemoteCSV('http://1.117.224.40/DEM/test.csv', (res) => {
-        const jsonData = res.data
-        const width = jsonData[0].length
-        const height = jsonData.length
+      readRemoteCSV('http://localhost:8080/example/DEM/2dDEM.csv', (res) => {
+        let jsonData = res.data
+        if (jsonData[jsonData.length - 1] == '') {
+          //去除最后的空行
+          jsonData.pop()
+        }
+        const width = jsonData[0].length //x方向数据数量
+        const height = jsonData.length //z向数据数量
+        // 创建y高度数据
         let data = new Float32Array(width * height)
         for (let i = 0; i < height; i++) {
           const rowData = jsonData[i]
@@ -41,6 +47,7 @@ export default {
             data[index] = heightData
           }
         }
+        console.log('进入init')
         this.init(width, height, data)
       })
     },
@@ -49,132 +56,139 @@ export default {
       const worldHalfWidth = worldWidth / 2,
         worldHalfDepth = worldDepth / 2
       this.container = document.getElementById('container')
-
+      // 渲染器对象
       this.renderer = new THREE.WebGLRenderer({ antialias: true })
       this.renderer.setSize(
         this.container.clientWidth,
         this.container.clientHeight
       )
-
+      // 添加canvas容器
       this.container.appendChild(this.renderer.domElement)
+      // 场景对象
       this.scene = new THREE.Scene()
       this.scene.background = new THREE.Color(0xbfd1e5)
-
+      // 摄像机对象
       this.camera = new THREE.PerspectiveCamera(
         60,
         this.container.clientWidth / this.container.clientHeight,
         50,
-        20000
+        200000
       )
-
+      // 拖拽控制器对象
       this.controls = new OrbitControls(this.camera, this.renderer.domElement)
       // this.controls.minDistance = 10
       // this.controls.maxDistance = 10000000
       this.controls.maxPolarAngle = Math.PI / 2
-
-      // const data = generateHeight(worldWidth, worldDepth)
       this.controls.target.y =
-        data[worldHalfWidth + worldHalfDepth * worldWidth] + 100
-      this.camera.position.y = this.controls.target.y + 200
-      this.camera.position.x = 200
+        data[worldHalfWidth + worldHalfDepth * worldWidth] + 500
+      this.camera.position.y = this.controls.target.y + 4000
+      this.camera.position.x = 5000
       this.controls.update()
-
+      // 定义几何对象
       const geometry = new THREE.PlaneGeometry(
-        750,
-        750,
+        worldWidth * 10,
+        worldDepth * 10,
         worldWidth - 1,
         worldDepth - 1
       )
       geometry.rotateX(-Math.PI / 2)
-      //* 这里直接修改plane的y坐标，即高度坐标
+      // 这里直接修改plane的y坐标，即高度坐标
       const vertices = geometry.attributes.position.array
-      console.log(geometry)
-      for (let i = 0, j = 0, l = vertices.length; i < l; i++, j += 3) {
-        vertices[j + 1] = data[i] * 10
+      for (let i = 0, l = vertices.length; i < l; i++) {
+        vertices[3 * i + 1] = data[i]
       }
-
-      this.texture = new THREE.CanvasTexture(
-        generateTexture(data, worldWidth, worldDepth)
-      )
-      this.texture.wrapS = THREE.ClampToEdgeWrapping
-      this.texture.wrapT = THREE.ClampToEdgeWrapping
-
+      geometry.computeVertexNormals()
+      // // 纹理贴图对象
+      // this.texture = new THREE.CanvasTexture(
+      //   generateTexture(data, worldWidth, worldDepth)
+      // )
+      // this.texture.wrapS = THREE.ClampToEdgeWrapping
+      // this.texture.wrapT = THREE.ClampToEdgeWrapping
+      // 组装成Mesh并添加到场景
       this.mesh = new THREE.Mesh(
         geometry,
-        new THREE.MeshBasicMaterial({ map: this.texture })
+        // new THREE.MeshPhongMaterial({ map: this.texture })
+        new THREE.MeshLambertMaterial({ color: 0xc7c7c7 })
       )
+      this.mesh.receiveShadow = true
+      this.mesh.castShadow = true
       this.scene.add(this.mesh)
+      // 光源对象
+      const light = new THREE.DirectionalLight(0xffffff, 1)
+      light.position.set(2500, 1500, 0)
+      this.scene.add(light)
 
+      // 动画循环
       animate()
 
-      function generateTexture(data, width, height) {
-        // bake lighting into texture
+      // function generateTexture(data, width, height) {
+      //   // bake lighting into texture
 
-        let context, image, imageData, shade
+      //   let context, image, imageData, shade
 
-        const vector3 = new THREE.Vector3(0, 0, 0)
+      //   const vector3 = new THREE.Vector3(0, 0, 0)
 
-        const sun = new THREE.Vector3(1, 1, 1)
-        sun.normalize()
+      //   const sun = new THREE.Vector3(1, 1, 1)
+      //   sun.normalize()
 
-        const canvas = document.createElement('canvas')
-        canvas.width = width
-        canvas.height = height
+      //   const canvas = document.createElement('canvas')
+      //   canvas.width = width
+      //   canvas.height = height
 
-        context = canvas.getContext('2d')
-        context.fillStyle = '#000'
-        context.fillRect(0, 0, width, height)
+      //   context = canvas.getContext('2d')
+      //   context.fillStyle = '#000'
+      //   context.fillRect(0, 0, width, height)
 
-        image = context.getImageData(0, 0, canvas.width, canvas.height)
-        imageData = image.data
+      //   image = context.getImageData(0, 0, canvas.width, canvas.height)
+      //   imageData = image.data
 
-        for (let i = 0, j = 0, l = imageData.length; i < l; i += 4, j++) {
-          vector3.x = data[j - 2] - data[j + 2]
-          vector3.y = 2
-          vector3.z = data[j - width * 2] - data[j + width * 2]
-          vector3.normalize()
+      //   for (let i = 0, j = 0, l = imageData.length; i < l; i += 4, j++) {
+      //     vector3.x = data[j - 2] - data[j + 2]
+      //     vector3.y = 2
+      //     vector3.z = data[j - width * 2] - data[j + width * 2]
+      //     vector3.normalize()
 
-          shade = vector3.dot(sun)
+      //     shade = vector3.dot(sun)
 
-          imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007)
-          imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007)
-          imageData[i + 2] = shade * 96 * (0.5 + data[j] * 0.007)
-        }
+      //     imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007)
+      //     imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007)
+      //     imageData[i + 2] = shade * 96 * (0.5 + data[j] * 0.007)
+      //   }
 
-        context.putImageData(image, 0, 0)
+      //   context.putImageData(image, 0, 0)
 
-        // Scaled 4x
+      //   // Scaled 4x
 
-        const canvasScaled = document.createElement('canvas')
-        canvasScaled.width = width * 4
-        canvasScaled.height = height * 4
+      //   const canvasScaled = document.createElement('canvas')
+      //   canvasScaled.width = width * 4
+      //   canvasScaled.height = height * 4
 
-        context = canvasScaled.getContext('2d')
-        context.scale(4, 4)
-        context.drawImage(canvas, 0, 0)
+      //   context = canvasScaled.getContext('2d')
+      //   context.scale(4, 4)
+      //   context.drawImage(canvas, 0, 0)
 
-        image = context.getImageData(
-          0,
-          0,
-          canvasScaled.width,
-          canvasScaled.height
-        )
-        imageData = image.data
+      //   image = context.getImageData(
+      //     0,
+      //     0,
+      //     canvasScaled.width,
+      //     canvasScaled.height
+      //   )
+      //   imageData = image.data
 
-        for (let i = 0, l = imageData.length; i < l; i += 4) {
-          const v = ~~(Math.random() * 5)
+      //   for (let i = 0, l = imageData.length; i < l; i += 4) {
+      //     const v = ~~(Math.random() * 5)
 
-          imageData[i] += v
-          imageData[i + 1] += v
-          imageData[i + 2] += v
-        }
+      //     imageData[i] += v
+      //     imageData[i + 1] += v
+      //     imageData[i + 2] += v
+      //   }
 
-        context.putImageData(image, 0, 0)
+      //   context.putImageData(image, 0, 0)
 
-        return canvasScaled
-      }
+      //   return canvasScaled
+      // }
       function animate() {
-        requestAnimationFrame(animate)
+        that.animationID = requestAnimationFrame(animate)
         that.renderer.render(that.scene, that.camera)
       }
     },
@@ -183,6 +197,14 @@ export default {
   mounted() {
     this.fetchData()
     // this.init(6, 6)
+  },
+  beforeDestroy() {
+    this.mesh.remove()
+    this.scene.clear()
+    this.renderer.dispose()
+    this.renderer.forceContextLoss()
+    this.renderer.content = null
+    cancelAnimationFrame(this.animationID)
   },
 }
 </script>
