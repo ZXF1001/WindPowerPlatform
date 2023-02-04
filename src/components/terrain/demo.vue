14098d9d6dbd24ad661f250b416a7cf48f9aca55
张潇峰 <1809996465@qq.com>
2023-02-04 20:54:44 +0800

    将一些代码封装成函数增加可读性

--- src/components/terrain/demo.vue
+++ src/components/terrain/demo.vue
@@ -14,9 +14,11 @@ export default {
     return {
       container: null,
       camera: null,
+      orthoCamera: null,
       lut: null,
       controls: null,
       scene: null,
+      uiScene: null,
       renderer: null,
       mesh: null,
       animationID: null,
@@ -25,7 +27,6 @@ export default {
   methods: {
     fetchTIFF() {
       // 使用匿名函数的写法
-
       (async () => {
         console.log('加载DEM')
         const url = 'http://1.117.224.40/geotiff/test_compress.tif'
@@ -39,25 +40,66 @@ export default {
     },
 
     createTHREE(boundingBox, rastersData) {
+      // 角度转弧度
       function D2R(deg) {
-        // 角度转弧度
         const rad = (deg * Math.PI) / 180
         return rad
       }
 
-      const init = () => {
+      // 模型真实尺寸计算，单位统一为m
+      function calcWH(boundingBox) {
         const lngMin = boundingBox[0]
         const lngMax = boundingBox[2]
         const latMin = boundingBox[1]
         const latMax = boundingBox[3]
 
         const earthRadius = 6371393 //地球半径，用于计算区域边界的真实距离
-        const geoWidth = Math.round(
+        const WEdistance = Math.round(
           earthRadius *
             Math.cos(D2R(latMin + latMax) / 2) *
             D2R(lngMax - lngMin)
         )
-        const geoHeight = Math.round(earthRadius * D2R(latMax - latMin))
+        const NSdistance = Math.round(earthRadius * D2R(latMax - latMin))
+        return { WEdistance, NSdistance }
+      }
+
+      // 这里直接修改plane的y坐标，即高度坐标，并将高度数据用于画图
+      function reshapePlane(geometry, data) {
+        const vertices = geometry.attributes.position.array
+        for (let i = 0; i < data.length; i++) {
+          vertices[3 * i + 1] = data[i]
+        }
+        geometry.attributes.position.array = vertices
+        const colors = new Array(3 * data.length).fill(1)
+        geometry.setAttribute(
+          'color',
+          new THREE.Float32BufferAttribute(colors, 3)
+        )
+        geometry.computeVertexNormals()
+        return geometry
+      }
+      // 获取typed array的最大最小值
+      function getMinAndMax(data) {
+        let min,
+          max = data[0]
+        for (let i = 0; i < data.length; i++) {
+          min = min < data[i] ? min : data[i]
+          max = max > data[i] ? max : data[i]
+        }
+        return { min, max }
+      }
+
+      function colorGeometry(data, geometry, lut) {
+        for (let i = 0; i < data.length; i++) {
+          const colorValue = data[i]
+          const rgb = lut.getColor(colorValue)
+          geometry.attributes.color.setXYZ(i, rgb.r, rgb.g, rgb.b)
+        }
+        geometry.attributes.color.needsUpdate = true
+      }
+
+      const init = (boundingBox, rastersData) => {
+        const { WEdistance, NSdistance } = calcWH(boundingBox)
         const rasterWidth = rastersData.width,
           rasterHeight = rastersData.height
         const data = rastersData[0]
@@ -82,7 +124,9 @@ export default {
         const near = 50
         const far = 500000
         this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-        this.camera.position.set(0, 0.8 * geoWidth, 0.8 * geoWidth) // 第二个坐标是高度，第三个坐标是前后
+        this.camera.position.set(0, 0.8 * WEdistance, 0.8 * WEdistance) // 第二个坐标是高度，第三个坐标是前后
+        this.orthoCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 1, 2)
+        this.orthoCamera.position.set(-0.9, 0, 1)
         // 拖拽控制器对象
         this.controls = new OrbitControls(this.camera, this.renderer.domElement)
         this.controls.minDistance = near
@@ -91,53 +135,32 @@ export default {
         this.controls.target.y = data[Math.floor(data.length / 2)] + 1000
         this.controls.update()
         // 定义几何对象
-        const geometry = new THREE.PlaneGeometry(
-          geoWidth,
-          geoHeight,
+        let geometry = new THREE.PlaneGeometry(
+          WEdistance,
+          NSdistance,
           rasterWidth - 1,
           rasterHeight - 1
         )
         geometry.rotateX(-Math.PI / 2)
-
-        // 这里直接修改plane的y坐标，即高度坐标
-        const vertices = geometry.attributes.position.array
-        const colors = []
-        for (let i = 0; i < data.length; i++) {
-          vertices[3 * i + 1] = data[i]
-          colors.push(1, 1, 1)
-        }
-
-        geometry.setAttribute(
-          'color',
-          new THREE.Float32BufferAttribute(colors, 3)
-        )
-        //设置lut
+        geometry = reshapePlane(geometry, data)
+        //设置颜色映射表
         this.uiScene = new THREE.Scene()
         this.lut = new Lut()
-
-        this.orthoCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 1, 2)
-        this.orthoCamera.position.set(-0.9, 0, 1)
+        this.lut.setColorMap('rainbow')
+        const { min, max } = getMinAndMax(data)
+        this.lut.setMax(max)
+        this.lut.setMin(min)
         const sprite = new THREE.Sprite(
           new THREE.SpriteMaterial({
             map: new THREE.CanvasTexture(this.lut.createCanvas()),
           })
         )
-        sprite.scale.x = 0.05
-
+        sprite.scale.x = 0.04
         this.uiScene.add(sprite)
-        //todo 需要自动选择最大最小值
-        this.lut.setColorMap('rainbow')
-        this.lut.setMax(2000)
-        this.lut.setMin(0)
-        geometry.computeVertexNormals()
+
         // 设置模型颜色
-        //todo 可以和上面的for循环合并
-        for (let i = 0; i < data.length; i++) {
-          const colorValue = data[i]
-          const color = this.lut.getColor(colorValue)
-          geometry.attributes.color.setXYZ(i, color.r, color.g, color.b)
-        }
-        geometry.attributes.color.needsUpdate = true
+
+        colorGeometry(data, geometry, this.lut)
 
         // 组装成Mesh
         this.mesh = new THREE.Mesh(
@@ -151,7 +174,7 @@ export default {
         this.mesh.castShadow = true
         this.scene.add(this.mesh)
         // 坐标轴指示，后面删掉
-        const axesHelper = new THREE.AxesHelper(geoHeight)
+        const axesHelper = new THREE.AxesHelper(NSdistance)
         this.scene.add(axesHelper)
         // 光源对象
         const light = new THREE.DirectionalLight('#fff', 1)
@@ -159,16 +182,20 @@ export default {
         light.castShadow = true
         this.scene.add(light)
 
-        const gridHelper = new THREE.GridHelper(1.25 * geoWidth, 50)
+        const gridHelper = new THREE.GridHelper(1.25 * WEdistance, 50)
         this.scene.add(gridHelper)
         // 自适应窗口尺寸
         window.onresize = () => {
+          const newAspect =
+            this.container.clientWidth / this.container.clientHeight
           this.renderer.setSize(
             this.container.clientWidth,
             this.container.clientHeight
           )
-          this.camera.aspect = aspect
+          this.camera.aspect = newAspect
           this.camera.updateProjectionMatrix()
+          this.orthoCamera.aspect = newAspect
+          this.orthoCamera.updateProjectionMatrix()
         }
       }
       // 动画循环
@@ -178,7 +205,7 @@ export default {
         this.renderer.render(this.uiScene, this.orthoCamera)
       }
 
-      init()
+      init(boundingBox, rastersData)
       animate()
     },
   },
@@ -189,6 +216,7 @@ export default {
   beforeDestroy() {
     this.mesh.remove()
     this.scene.clear()
+    this.uiScene.clear()
     this.renderer.dispose()
     this.renderer.forceContextLoss()
     this.renderer.content = null
